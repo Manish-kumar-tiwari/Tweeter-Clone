8c74d78877a90955f938ddaf25339f8568f7557c
Manish <mt5155614@gmail.com>
2024-07-11 20:35:13 +0530

    sixth commit

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -3,16 +3,31 @@ import CreatePost from "./CreatePost";
 import Tweet from "./Tweet";
 import { useDispatch, useSelector } from "react-redux";
 import { setFollowing } from "./redux/tweetSlice";
+import Avatar from "react-avatar";
 
 const Feed = () => {
   const { allTweets, followingTweets, following } = useSelector(
     (store) => store.tweet
   );
 
+  const { user } = useSelector((store) => store.user);
+
   const dispatch = useDispatch();
 
   return (
     <div className="ml-14 max-[800px]:ml-0 max-[800px]:w-[100%] w-[50%]  border-l border-t border-r border-gray-900 h-auto  ">
+      <div className="min-[800px]:hidden">
+        <div className="flex items-center gap-24">
+          <div className="p-3 ">
+            <Avatar src={user?.avatar} size="54px" round={true} />
+          </div>
+          <img
+            width={"50px"}
+            src="https://freelogopng.com/images/all_img/1690643591twitter-x-logo-png.png"
+            alt="twitter-logo"
+          />
+        </div>
+      </div>
       <div className="flex justify-evenly  border-b border-gray-900  ">
         <div
           onClick={() => {
